860605d927af6724dab273413060379643e4fa7d
cluo1 <ln37807090>
2019-07-24 16:42:28 +0800

    user

--- sql/properties.sql
+++ sql/properties.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50640
 File Encoding         : 65001
 
-Date: 2019-07-24 14:47:53
+Date: 2019-07-24 16:31:49
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -31,23 +31,24 @@ CREATE TABLE `properties` (
 -- ----------------------------
 -- Records of properties
 -- ----------------------------
-INSERT INTO `properties` VALUES ('geteway-server', 'eureka.client.serviceUrl.defaultZone', 'http://localhost:8000/eureka/', 'dev', 'master');
-INSERT INTO `properties` VALUES ('geteway-server', 'server.port', '8020', 'dev', 'master');
-INSERT INTO `properties` VALUES ('geteway-server', 'spring.cloud.bus.enabled', 'true', 'dev', 'master');
-INSERT INTO `properties` VALUES ('geteway-server', 'spring.cloud.bus.trace.enableds', 'true', 'dev', 'master');
-INSERT INTO `properties` VALUES ('geteway-server', 'spring.rabbitmq.host', 'localhost', 'dev', 'master');
-INSERT INTO `properties` VALUES ('geteway-server', 'spring.rabbitmq.port', '5672', 'dev', 'master');
-INSERT INTO `properties` VALUES ('geteway-server', 'spring.rabbitmq.username', 'guest', 'dev', 'master');
-INSERT INTO `properties` VALUES ('geteway-server', 'spring.rabbitmq.password', 'guest', 'dev', 'master');
-INSERT INTO `properties` VALUES ('geteway-server', 'management.endpoints.web.exposure.include', '*', 'dev', 'master');
-INSERT INTO `properties` VALUES ('geteway-server', 'management.endpoints.web.cors.allowed-methods', '*', 'dev', 'master');
-INSERT INTO `properties` VALUES ('geteway-server', 'management.endpoints.web.cors.allowed-origins', '*', 'dev', 'master');
-INSERT INTO `properties` VALUES ('geteway-server', 'zuul.ignored-services', '*', 'dev', 'master');
-INSERT INTO `properties` VALUES ('geteway-server', 'zuul.routes.config-server', '/config-service/**', 'dev', 'master');
-INSERT INTO `properties` VALUES ('geteway-server', 'zuul.routes.demo-server', '/demo-service/**', 'dev', 'master');
-INSERT INTO `properties` VALUES ('geteway-server', 'zuul.routes.feign-server', '/feign-service/**', 'dev', 'master');
 
+INSERT INTO `properties` VALUES ('gateway-server', 'management.endpoints.web.cors.allowed-methods', '*', 'dev', 'master');
+INSERT INTO `properties` VALUES ('gateway-server', 'management.endpoints.web.cors.allowed-origins', '*', 'dev', 'master');
+INSERT INTO `properties` VALUES ('gateway-server', 'management.endpoints.web.exposure.include', '*', 'dev', 'master');
+INSERT INTO `properties` VALUES ('gateway-server', 'server.port', '8020', 'dev', 'master');
+INSERT INTO `properties` VALUES ('gateway-server', 'spring.cloud.bus.enabled', 'true', 'dev', 'master');
+INSERT INTO `properties` VALUES ('gateway-server', 'spring.cloud.bus.trace.enableds', 'true', 'dev', 'master');
+INSERT INTO `properties` VALUES ('gateway-server', 'spring.rabbitmq.host', 'localhost', 'dev', 'master');
+INSERT INTO `properties` VALUES ('gateway-server', 'spring.rabbitmq.password', 'guest', 'dev', 'master');
+INSERT INTO `properties` VALUES ('gateway-server', 'spring.rabbitmq.port', '5672', 'dev', 'master');
+INSERT INTO `properties` VALUES ('gateway-server', 'spring.rabbitmq.username', 'guest', 'dev', 'master');
+INSERT INTO `properties` VALUES ('gateway-server', 'zuul.ignored-services', '*', 'dev', 'master');
+INSERT INTO `properties` VALUES ('gateway-server', 'zuul.routes.config-server', '/config/**', 'dev', 'master');
+INSERT INTO `properties` VALUES ('gateway-server', 'zuul.routes.demo-server', '/demo-service/**', 'dev', 'master');
+INSERT INTO `properties` VALUES ('gateway-server', 'zuul.routes.feign-server', '/feign-service/**', 'dev', 'master');
 
 INSERT INTO `properties` VALUES ('demo-server', 'server.port', '8081', 'dev', 'master');
-INSERT INTO `properties` VALUES ('demo-server', 'eureka.client.serviceUrl.defaultZone', 'http://localhost:8000/eureka/', 'dev', 'master');
 
+INSERT INTO `properties` VALUES ('feign-server', 'feign.hystrix.enabled', 'true', 'dev', 'master');
+INSERT INTO `properties` VALUES ('feign-server', 'server.connection-timeout', '10000', 'dev', 'master');
+INSERT INTO `properties` VALUES ('feign-server', 'server.port', '8082', 'dev', 'master');
