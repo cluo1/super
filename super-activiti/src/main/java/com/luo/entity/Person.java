d3b71e20afff49f9604dbdeec1cd0fe938042ad0
cluo1 <ln37807090>
2019-08-06 15:09:07 +0800

    modify

---
+++ super-activiti/src/main/java/com/luo/entity/Person.java
@@ -0,0 +1,42 @@
+package com.luo.entity;
+
+import java.util.Date;
+
+public class Person {
+    private Integer id;
+    private String name;
+    private String note;
+    private Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
