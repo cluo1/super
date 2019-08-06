5c8efadd37a658d9db0aaaa1eafacc2155adf6ea
cluo1 <ln37807090>
2019-08-06 15:25:11 +0800

    modify

---
+++ super-activiti/src/main/java/com/luo/SuperActivitiApplication.java
@@ -0,0 +1,13 @@
+package com.luo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperActivitiApplication.class, args);
+    }
+
+}
