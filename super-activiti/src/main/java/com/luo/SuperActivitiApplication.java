345d54ae0c8c40e3ae18ea74ed6c1241d5e4c8af
cluo1 <ln37807090>
2019-08-07 10:54:54 +0800

    a

--- super-activiti/src/main/java/com/luo/SuperActivitiApplication.java
+++ super-activiti/src/main/java/com/luo/SuperActivitiApplication.java
@@ -3,8 +3,10 @@ package com.luo;
 import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableEurekaClient
 public class SuperActivitiApplication {
 
     public static void main(String[] args) {
