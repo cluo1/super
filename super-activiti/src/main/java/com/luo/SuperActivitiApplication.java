fcfebaf954ffe9da4e942e83d8e400c330be8ee3
cluo1 <ln37807090>
2019-08-08 15:54:13 +0800

    b

--- super-activiti/src/main/java/com/luo/SuperActivitiApplication.java
+++ super-activiti/src/main/java/com/luo/SuperActivitiApplication.java
@@ -12,6 +12,7 @@ public class SuperActivitiApplication {
     public static void main(String[] args) {
         SpringApplication.run(SuperActivitiApplication.class, args);
         System.out.println("a");
+        System.out.println("b");
     }
 
 }
