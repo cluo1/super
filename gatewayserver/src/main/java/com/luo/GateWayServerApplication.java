aca6e022ce380e1cd00583eae9493a65adb15e60
cluo1 <ln37807090>
2019-11-14 17:21:40 +0800

    zuul升级为gateway

--- gatewayserver/src/main/java/com/luo/GateWayServerApplication.java
+++ gatewayserver/src/main/java/com/luo/GateWayServerApplication.java
@@ -3,11 +3,10 @@ package com.luo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableZuulProxy
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class GateWayServerApplication {
 
     public static void main(String[] args) {
--- sql/super_properties.sql
+++ sql/super_properties.sql
@@ -1,4 +1,4 @@
-/*
+a/*
 Navicat MySQL Data Transfer
 
 Source Server         : super
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50640
 File Encoding         : 65001
 
-Date: 2019-07-24 19:34:41
+Date: 2019-11-14 16:05:45
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -31,21 +31,11 @@ CREATE TABLE `super_properties` (
 -- ----------------------------
 -- Records of super_properties
 -- ----------------------------
-INSERT INTO `super_properties` VALUES ('demo-server', 'server.port', '8081', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('feign-server', 'feign.hystrix.enabled', 'true', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('feign-server', 'server.connection-timeout', '10000', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('feign-server', 'server.port', '8081', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('gateway-server', 'management.endpoints.web.cors.allowed-methods', '*', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('gateway-server', 'management.endpoints.web.cors.allowed-origins', '*', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('gateway-server', 'management.endpoints.web.exposure.include', '*', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('gateway-server', 'server.port', '8020', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('gateway-server', 'spring.cloud.bus.enabled', 'true', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('gateway-server', 'spring.cloud.bus.trace.enableds', 'true', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('gateway-server', 'spring.rabbitmq.host', 'localhost', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('gateway-server', 'spring.rabbitmq.password', 'guest', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('gateway-server', 'spring.rabbitmq.port', '5672', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('gateway-server', 'spring.rabbitmq.username', 'guest', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('gateway-server', 'zuul.ignored-services', '*', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('gateway-server', 'zuul.routes.config-server', '/config/**', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('gateway-server', 'zuul.routes.demo-server', '/demo/**', 'dev', 'master');
-INSERT INTO `super_properties` VALUES ('gateway-server', 'zuul.routes.feign-server', '/feign/**', 'dev', 'master');
+INSERT INTO `super_properties` VALUES (''demo'', ''server.port'', ''8081'', ''dev'', ''master'');
+INSERT INTO `super_properties` VALUES (''feign'', ''feign.hystrix.enabled'', ''true'', ''dev'', ''master'');
+INSERT INTO `super_properties` VALUES (''feign'', ''server.connection-timeout'', ''10000'', ''dev'', ''master'');
+INSERT INTO `super_properties` VALUES (''feign'', ''server.port'', ''8081'', ''dev'', ''master'');
+INSERT INTO `super_properties` VALUES (''gateway'', ''management.endpoints.web.cors.allowed-methods'', ''*'', ''dev'', ''master'');
+INSERT INTO `super_properties` VALUES (''gateway'', ''management.endpoints.web.cors.allowed-origins'', ''*'', ''dev'', ''master'');
+INSERT INTO `super_properties` VALUES (''gateway'', ''management.endpoints.web.exposure.include'', ''*'', ''dev'', ''master'');
+INSERT INTO `super_properties` VALUES (''gateway'', ''server.port'', ''8020'', ''dev'', ''master'');
