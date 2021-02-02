dc0ecea6600dc163260a75ba36ad4915252c30ee
cluo199 <cluo199@163.com>
2021-02-02 16:56:05 +0800

    test

--- super-redis/src/main/java/com/luo/RedisApplication.java
+++ super-redis/src/main/java/com/luo/RedisApplication.java
@@ -2,8 +2,10 @@ package com.luo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { RedisRepositoriesAutoConfiguration.class, MongoRepositoriesAutoConfiguration.class })
 public class RedisApplication {
 
     public static void main(String[] args) {
