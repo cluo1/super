864594a08d33c4ce55fa878c7738f9235e51fb0d
cluo199 <cluo199@163.com>
2021-02-08 11:17:40 +0800

    kafka组件

---
+++ super-demo/src/test/java/com/luo/kafka/KafkaTest.java
@@ -0,0 +1,20 @@
+//package com.luo.kafka;
+//
+//import com.luo.DemoApplication;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = DemoApplication.class)
+//public class KafkaTest {
+//
+//    @Autowired
+//    private KafkaProducer kafkaProducer;
+//    @Test
+//    public void send(){
+//        kafkaProducer.send();
+//    }
+//}
--- super-demo/src/test/java/com/luo/zk/zkTest.java
+++ super-demo/src/test/java/com/luo/zk/zkTest.java
@@ -1,34 +1,34 @@
-package com.luo.zk;
-
-import com.luo.BaseLockHandler;
-import com.luo.DemoApplication;
-import com.luo.ShardReentrantLockComponent;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = DemoApplication.class)
-public class zkTest {
-    
-    @Autowired
-    private ShardReentrantLockComponent lockComponent;
-
-    @Test
-    public void test(){
-        String path="/super";
-        boolean existNode = lockComponent.isExistNode(path);
-        String nodeData = lockComponent.getNodeData(path);
-
-        //分布式锁
-        lockComponent.acquireLock(new BaseLockHandler<Object>(path) {
-            @Override
-            public boolean handler() {
-                //业务处理
-                return false;
-            }
-        });
-    }
-}
+//package com.luo.zk;
+//
+//import com.luo.BaseLockHandler;
+//import com.luo.DemoApplication;
+//import com.luo.ShardReentrantLockComponent;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest(classes = DemoApplication.class)
+//public class zkTest {
+//
+//    @Autowired
+//    private ShardReentrantLockComponent lockComponent;
+//
+//    @Test
+//    public void test(){
+//        String path="/super";
+//        boolean existNode = lockComponent.isExistNode(path);
+//        String nodeData = lockComponent.getNodeData(path);
+//
+//        //分布式锁
+//        lockComponent.acquireLock(new BaseLockHandler<Object>(path) {
+//            @Override
+//            public boolean handler() {
+//                //业务处理
+//                return false;
+//            }
+//        });
+//    }
+//}
