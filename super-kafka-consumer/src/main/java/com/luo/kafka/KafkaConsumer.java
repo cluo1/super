6e117f4d8b2b5c8afbe671c02b698487379594eb
cluo199 <cluo199@163.com>
2021-06-21 18:23:50 +0800

    test

--- super-kafka-consumer/src/main/java/com/luo/kafka/KafkaConsumer.java
+++ super-kafka-consumer/src/main/java/com/luo/kafka/KafkaConsumer.java
@@ -12,35 +12,32 @@ import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 @Component
 public class KafkaConsumer {
     private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
 
-    @KafkaListener(topics = "indexMsgTopic")
+    @KafkaListener(topics = "indexMsgTopic"
+            ,group = "g1"
+            ,containerFactory = "kafkaListenerContainerFactory1"
+    )
     public void consumer1(ConsumerRecord consumerRecord){
-        long offset = consumerRecord.offset();
         Object value = consumerRecord.value();
 
         LinkedHashMap infoList = JSONObject.parseObject(value+"",new TypeReference<LinkedHashMap>() {});
         logger.info("接收任务索引消息数据开始voiceId:{},ruleId:{}",infoList.get("voiceId"),infoList.get("ruleId"));
-
-//        logger.info("consumer1===========offset:{},val:{}",offset,infoList);
     }
 
 
- /*   @KafkaListener(topicPartitions ={@TopicPartition(topic = "topic1",partitions = {"0","1"}
-//        ,partitionOffsets = @PartitionOffset(partition = "1", initialOffset = "4")
-        )}
-        ,containerFactory = "kafkaListenerContainerFactory1"
-//        ,groupId = "g2"
+    //同一个分区同一个消费组中不同的消费者只能有一个消费者消费 保证顺序消费
+    @KafkaListener(topics = "indexMsgTopic"
+            ,group = "g1"
+//           ,topicPartitions ={@TopicPartition(topic = "indexMsgTopic",partitions = {"0"})}
+            ,containerFactory = "kafkaListenerContainerFactory1"
     )
     public void consumer2(ConsumerRecord consumerRecord){
         long offset = consumerRecord.offset();
         String val = JSONObject.toJSONString(consumerRecord.value());
-        log.info("consumer2===========offset:{},val:{}",offset,val);
+        logger.info("consumer===========offset:{},val:{}",offset,val);
     }
-*/
 }
--- super-kafka-consumer/src/main/java/com/luo/kafka/KafkaConsumerConfig.java
+++ super-kafka-consumer/src/main/java/com/luo/kafka/KafkaConsumerConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
@@ -14,7 +15,7 @@ import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import java.util.HashMap;
 import java.util.Map;
 
-//@Configuration
+@Configuration
 public class KafkaConsumerConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
@@ -50,8 +51,8 @@ public class KafkaConsumerConfig {
         public Map<String, Object> getCommonPropertis() {
         Map<String, Object> properties = new HashMap<String, Object>();
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
-        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1");
         properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
